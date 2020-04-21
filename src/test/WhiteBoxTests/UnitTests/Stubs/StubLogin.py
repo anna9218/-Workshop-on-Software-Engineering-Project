cff8c59ce96d6a4c0af32607491cf0fb42bc53ba
annaM <annamiro@post.bgu.ac.il>
2020-04-21 21:42:23 +0300

    login and logout unit tests

---
+++ src/test/WhiteBoxTests/UnitTests/Stubs/StubLogin.py
@@ -0,0 +1,10 @@
+class StubLogin:
+
+    def __init__(self):
+        self.isLoggedIn = False
+
+    def login(self, user, username, password):
+        if username == "anna9218" and password == "password":
+            self.isLoggedIn = True
+        else:
+            self.isLoggedIn = False
