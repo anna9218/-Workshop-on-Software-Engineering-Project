f9151d2847792654ce270a40275625e9d5062102
eytaniva <eytaniva@post.bgu.ac.il>
2020-04-24 15:20:26 +0300

    Test for Einat

--- src/test/WhiteBoxTests/UnitTests/TestCases/FacadePaymentTests.py
+++ src/test/WhiteBoxTests/UnitTests/TestCases/FacadePaymentTests.py
@@ -1,5 +1,6 @@
 import unittest
 from src.main.DomainLayer.FacadePayment import FacadePayment
+from datetime import datetime as date_time
 
 
 class FacadePaymentTests(unittest.TestCase):
@@ -8,9 +9,10 @@ class FacadePaymentTests(unittest.TestCase):
         self.__payment_sys = FacadePayment.get_instance()
         self.__payment_sys.connect()
         self.__valid_username = "username"
-        self.__valid_date = "22/4/2020"
+        self.__valid_date = date_time(2021, 12, 21)
         self.__valid_credit = "123456789"
         self.__wrong_input = ""
+        self.__passed_date = date_time(2012, 12, 21)
 
     def test_connection(self):
         # test that system is connected fine
@@ -23,6 +25,9 @@ class FacadePaymentTests(unittest.TestCase):
     def test_wrong_input(self):
         # valid user, amount, credit + invalid date
         res = self.__payment_sys.commit_payment(self.__valid_username, 10, self.__valid_credit, self.__wrong_input)
+        self.assertEqual(False, res)
+        # valid user, amount, credit + invalid date
+        res = self.__payment_sys.commit_payment(self.__valid_username, 10, self.__valid_credit, self.__passed_date)
         self.assertEqual(False, res)
         # valid user, amount, date + invalid credit
         res = self.__payment_sys.commit_payment(self.__valid_username, 10, self.__wrong_input, self.__valid_date)
