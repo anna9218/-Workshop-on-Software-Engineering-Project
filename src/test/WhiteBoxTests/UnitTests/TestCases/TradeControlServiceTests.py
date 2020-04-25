3677631e7a3299bae3baedb239f41b42ab12ca57
annaM <annamiro@post.bgu.ac.il>
2020-04-25 13:57:17 +0300

    added a structure for all unit tests

---
+++ src/test/WhiteBoxTests/UnitTests/TestCases/TradeControlServiceTests.py
@@ -0,0 +1,16 @@
+import unittest
+
+
+class TradeControlServiceTests(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_init_system(self):
+        # TODO
+        pass
+
+    if __name__ == '__main__':
+        unittest.main()
