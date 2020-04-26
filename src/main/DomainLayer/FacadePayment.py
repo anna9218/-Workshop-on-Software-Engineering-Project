fec31ff8d068776b7e42ff40b3c5877268a1aef7
yarin9 <yarinc@post.bgu.ac.il>
2020-04-26 03:53:41 +0300

    log

--- src/main/DomainLayer/FacadePayment.py
+++ src/main/DomainLayer/FacadePayment.py
@@ -63,7 +63,7 @@ class FacadePayment:
         if type(date) != date_time:
             return False
 
-        if len(name) == 0 or len(credit) == 0 or (date_time.today().date() > date) or amount <= 0:
+        if len(name) == 0 or len(credit) == 0 or (date_time.today().date() > date.date()) or amount <= 0:
             return False
         else:
             return True
