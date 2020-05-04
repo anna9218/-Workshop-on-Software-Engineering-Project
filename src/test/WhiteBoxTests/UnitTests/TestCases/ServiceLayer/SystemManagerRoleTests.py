01cc5a96bf945330db819d7069e3ac824a51614f
annaM <annamiro@post.bgu.ac.il>
2020-05-05 00:42:04 +0300

    SystemManagerRoleTests with Mock - done

--- src/test/WhiteBoxTests/UnitTests/TestCases/ServiceLayer/SystemManagerRoleTests.py
+++ src/test/WhiteBoxTests/UnitTests/TestCases/ServiceLayer/SystemManagerRoleTests.py
@@ -1,8 +1,8 @@
 import unittest
+from unittest.mock import MagicMock
 
 from src.Logger import logger
-from src.main.DomainLayer.StoreComponent.Purchase import Purchase
-from src.main.ServiceLayer.GuestRole import TradeControl, User, Store
+from src.main.ServiceLayer.GuestRole import TradeControl
 from src.main.ServiceLayer.SystemManagerRole import SystemManagerRole
 
 
@@ -10,67 +10,33 @@ class SystemManagerRoleTests(unittest.TestCase):
     # @logger
     def setUp(self):
         self.__system_manager_role = SystemManagerRole()
-
-        self.__manager = User()
-        self.__manager.register("manager", "password")
-        (TradeControl.get_instance()).subscribe(self.__manager)
-        (TradeControl.get_instance()).add_system_manager(self.__manager)
-
-        self.__store_owner = User()
-        self.__store_owner.register("store_owner", "password")
-        (TradeControl.get_instance()).subscribe(self.__store_owner)
-
-        TradeControl.get_instance().set_curr_user(self.__store_owner)
-
-        self.__store_owner.login("store_owner", "password")
-        res = (TradeControl.get_instance()).open_store("Some Store")
-        self.__store: Store = (TradeControl.get_instance()).get_store("Some Store")
-        self.__store.add_products("store_owner", [{"name": "Chair", "price": 100, "category": "Furniture", "amount": 5},
-                                                          {"name": "Sofa", "price": 100, "category": "Furniture", "amount": 5}])
-        self.__purchase: Purchase = Purchase(777, ["some product", 1], 10, "Some Store", "manager")
-
-        self.__store_owner.add_accepted_purchase(self.__purchase)
-        self.__store.add_purchase(self.__purchase)
-
-        TradeControl.get_instance().set_curr_user(self.__system_manager_role)
+        self.__trade_control_mock = TradeControl().get_instance()
+        self.__viewed_user = "anna9218"
+        self.__store_name = "Some Store"
 
     # @logger
     def test_view_user_purchases_history(self):
-        # purchases = self.__store_owner.get_accepted_purchases()
+        self.__trade_control_mock.get_instance().view_user_purchase_history = MagicMock(return_value=True)
+        res = self.__system_manager_role.view_user_purchase_history(self.__viewed_user)
+        self.assertTrue(res)
 
-        # All valid - store_owner has a single purchase
-        result = self.__system_manager_role.view_user_purchase_history("store_owner")  # BUGBUG
-        self.assertTrue(result[0].get_purchase_id() == 777)
-        self.assertTrue(len(result) == 1)
-
-        # User with no purchases
-        result = self.__system_manager_role.view_user_purchase_history(self.__manager.get_nickname())
-        self.assertEqual(result, [])
-
-        # Invalid username
-        result = self.__system_manager_role.view_user_purchase_history("invalid_username")
-        self.assertIsNone(result)
-
-        self.assertEqual(self.__store_owner.get_accepted_purchases(), purchases)
+        self.__trade_control_mock.get_instance().view_user_purchase_history = MagicMock(return_value=False)
+        res = self.__system_manager_role.view_user_purchase_history(self.__viewed_user)
+        self.assertFalse(res)
 
     # @logger
     def test_view_store_purchases_history(self):
-        purchases = self.__store.get_purchases("store_owner")
+        self.__trade_control_mock.get_instance().view_store_purchases_history = MagicMock(return_value=True)
+        res = self.__system_manager_role.view_store_purchases_history(self.__store_name)
+        self.assertTrue(res)
 
-        # All valid
-        result = self.__system_manager_role.view_store_purchases_history(self.__store.get_name())
-        self.assertTrue(result[0].get_purchase_id() == 0)
+        self.__trade_control_mock.get_instance().view_store_purchases_history = MagicMock(return_value=False)
+        res = self.__system_manager_role.view_store_purchases_history(self.__store_name)
+        self.assertFalse(res)
 
-        # Store with no purchases
-        (TradeControl.get_instance()).open_store("Other Store")
-        result = self.__system_manager_role.view_store_purchases_history("Other Store")
-        self.assertEqual(len(result), 0)
-
-        # Invalid store name
-        result = self.__system_manager_role.view_store_purchases_history("invalid_username")
-        self.assertIsNone(result)
-
-        self.assertEqual(self.store_owner.get_accepted_purchases(), purchases)
+    # @logger
+    def tearDown(self):
+        self.__trade_control_mock.__delete__()
 
     def __repr__(self):
         return repr("SystemManagerRoleTests")
